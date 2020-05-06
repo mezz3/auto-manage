b756684cb8631cc6df9c9837b5b8ee736fb555cf
ChanakanMaa <Ninkk.CM@gmail.com>
2020-05-07 00:50:30 +0700

    change script clone vm

--- mysite/static/Scripts/createVM.ps1
+++ mysite/static/Scripts/createVM.ps1
@@ -2,7 +2,7 @@ $vCenter="10.0.15.39"
 $vCenterUser="Administrator@vsphere.local"
 $vCenterUserPassword="Qwerty1@#$"
 
-$vm_count = "2"
+$vm_count = 5
 
 $clone = "GNS3 VM"
 
@@ -12,15 +12,19 @@ $VM_prefix = "gns3vm-"
 
 write-host "Connecting to vCenter Server $vCenter" -foreground green
 Connect-viserver $vCenter -user $vCenterUser -password $vCenterUserPassword -WarningAction 0
-1..$vm_count | foreach {
-$y="{0:D1}" -f + $_
-$VM_name= $VM_prefix + $y
-Get-VMHost -state connected | Get-Random
-write-host "Creation of VM $VM_name initiated" -foreground green
-New-VM -Name $VM_Name -VM $clone -VMHost $Cluster -DiskStorageFormat Thin -RunAsync
+# $vm_count | foreach {
+# $y="{0:D1}" -f + $_
+# $VM_name= $VM_prefix + $y
+# Get-VMHost -state connected | Get-Random
+# write-host "Creation of VM $VM_name initiated" -foreground green
+# New-VM -Name $VM_Name -VM $clone -VMHost $Cluster -DiskStorageFormat Thin -RunAsync
+# }
+For ($i=1; $i -le $vm_count; $i++) {
+    $VM_name= $VM_prefix + $i
+    Get-VMHost -state connected | Get-Random
+    write-host "Creation of VM $VM_name initiated" -foreground green
+    New-VM -Name $VM_Name -VM $clone -VMHost $Cluster -DiskStorageFormat Thin -RunAsync
 }
-
-
 
 # $vCenter="10.0.15.39"
 # $vCenterUser="Administrator@vsphere.local"
