6a7e2d78bec5a1ce4c5c5c8b2692baf54e8eacb9
ChanakanMaa <Ninkk.CM@gmail.com>
2020-05-18 03:24:47 +0700

    url link topo to gns3 web

--- mysite/nwtopo/urls.py
+++ mysite/nwtopo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+# from django.conf.urls import patterns, url
+from django.views.generic import RedirectView
 
 from . import views
 
@@ -14,6 +16,9 @@ urlpatterns = [
     path('deploy/deploy_temp/succ/<str:temp_name>', views.deploy_temp_succ, name='deploy_temp_succ'),
     path('ip_report/', views.report, name='report'),
     path('ip_report/delete/<str:deploy_name>', views.deploy_del, name='deploy_del'),
+
+    path(r'^remote_admin/$', RedirectView.as_view(url='http://10.0.15.21/static/web-ui/server/1/project/9c278930-5a87-4b14-b2fb-7a11bd957e08'),
+        name='remote_admin'),
 ]
 
 if settings.DEBUG:
